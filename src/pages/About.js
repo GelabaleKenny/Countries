f79e74ce40336d10caec611667b6a9de0a24b7cc
Gelabale Kenny <kenny.gelabale@yahoo.com>
2023-02-23 18:01:04 +0100

    readme

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,11 +10,9 @@ const Aboute = () => {
       <h1>A propos</h1>
       <br />
       <p>
-        Voici un site réferencent tout les pays.
-        <br />
-        amusez vous avec vos amis a trouver les particularité de chaque pays et
-        testez ainsi votre culture générale.
-        <br />
+        Voici un site référencent tous les pays. <br />
+        amusez-vous avec vos amis à trouver les particularité de chaque pays et
+        testez ainsi votre culture générale. <br />
         Ludique et amusant ce petit jeu va agrémenter vos soirée raclette.
       </p>
     </div>
